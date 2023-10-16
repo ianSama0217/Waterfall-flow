2692c46a9d6b7483d2ae9339942ea7a95babe6f1
ianSama0217 <ian20000217@gmail.com>
2023-10-16 16:33:38 +0800

    Waterfall flow demo

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref, reactive } from "vue";
+import navbar from "./components/navbar.vue";
+
+const imgURLArr = [
+  `../public/img/pockmon1.jpg`,
+  `../public/img/pockmon2.jpg`,
+  `../public/img/pockmon3.jpg`,
+  `../public/img/pockmon4.jpg`,
+  `../public/img/pockmon5.jpg`,
+  `../public/img/pockmon6.jpg`,
+  `../public/img/pockmon7.jpg`,
+  `../public/img/pockmon8.jpg`,
+  `../public/img/pockmon9.jpg`,
+  `../public/img/pockmon10.jpg`,
+  `../public/img/pockmon11.jpg`,
+  `../public/img/pockmon12.jpg`,
+];
+
+const displayWaterfallFlow = () => {};
+
+window.addEventListener("load", () => {
+  displayWaterfallFlow();
+});
+</script>
+
+<template>
+  <navbar />
+
+  <!-- 瀑布流圖片 -->
+  <div class="imgArea">
+    <img
+      class="pockmonImg"
+      v-for="item in imgURLArr"
+      :src="item"
+      alt="神奇寶貝"
+      title="神奇寶貝"
+    />
+  </div>
+
+  <!-- ?icon -->
+  <img src="../public/icon/question-icon.svg" class="icon" title="更多" />
+</template>
+
+<style scoped>
+.imgArea {
+  padding: 0 10%;
+  /* 將欄位分成3等分 */
+  column-count: 3;
+
+  .pockmonImg {
+    max-width: 100%;
+    height: auto;
+    margin: 0.5rem;
+    border-radius: 8px;
+  }
+}
+
+.icon {
+  height: 4vw;
+  width: 4vw;
+  padding: 0.5rem;
+  border-radius: 50%;
+  /*水平偏移:0 垂直偏移:3px 模糊半徑:6px 擴散半徑:3px(數值為負，使陰影收縮到元素內部) 顏色:#777*/
+  box-shadow: 0 3px 6px 3px #777;
+  position: fixed;
+  bottom: 1%;
+  right: 1%;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <div class="navbar">
+    <img src="../../public/icon/house-icon.svg" class="h-icon" />
+    <span class="home">首頁</span>
+    <div class="bluid">
+      <span class="addtext">建立</span>
+      <img src="../../public/icon/arror-down-icon.svg" class="l-icon" />
+    </div>
+    <div class="serachBox">
+      <img src="../../public/icon/search-icon.svg" class="l-icon" />
+      <input type="text" placeholder="搜尋" class="searchBar" />
+    </div>
+    <img src="../../public/icon/bell-icon.svg" class="icon" />
+    <img src="../../public/icon/message-icon.svg" class="icon" />
+    <img src="../../public/icon/arror-down-icon.svg" class="icon" />
+  </div>
+</template>
+
+<style scoped>
+.h-icon {
+  height: 4vw;
+  width: 4vw;
+}
+.icon {
+  height: 4vw;
+  width: 4vw;
+  border-radius: 50%;
+  padding: 0.5rem;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #c0c0c0;
+    cursor: pointer;
+  }
+}
+
+.l-icon {
+  height: 2vw;
+  width: 2vw;
+}
+.navbar {
+  height: 15vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+
+  .home {
+    padding: 0.5rem 1rem;
+    border-radius: 20px;
+    background-color: black;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: bold;
+    transition: all 0.5s ease;
+
+    &:hover {
+      background-color: #c0c0c0;
+      color: black;
+      cursor: pointer;
+    }
+  }
+
+  .bluid {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    border-radius: 20px;
+    transition: all 0.5s ease;
+
+    &:hover {
+      background-color: #777;
+      color: white;
+      cursor: pointer;
+    }
+
+    .addtext {
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+    .icon {
+      margin-left: 0.5rem;
+    }
+  }
+
+  .serachBox {
+    display: flex;
+    position: relative;
+    .l-icon {
+      position: absolute;
+      top: 25%;
+      left: 3%;
+    }
+    .searchBar {
+      width: 30rem;
+      height: 3rem;
+      background-color: rgb(219, 219, 219);
+      border: none;
+      border-radius: 20px;
+      font-size: 1.35rem;
+      padding-left: 3rem;
+    }
+  }
+}
+</style>
